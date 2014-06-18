42766e0230cdc5466f74ba7ccf22e195087f338e
Nick Christidis <nick.christidis@yahoo.com>
2014-06-19 02:02:02 +0300

    Ta models gia tin vasi dedomenwn.....

--- src/database/connection/DbConnection.java
+++ src/database/connection/DbConnection.java
@@ -1,20 +1,18 @@
 package database.connection;
 
+import com.mysql.jdbc.Connection;
+
 /**
  *
  * @author nikos
  */
 public class DbConnection {
 
+    private static final String DB_URL = "";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "nikos";
+
     private DbConnection() {
     }
 
-    public static DbConnection getInstance() {
-        return DbConnectionHolder.INSTANCE;
-    }
-
-    private static class DbConnectionHolder {
-
-        private static final DbConnection INSTANCE = new DbConnection();
-    }
-}
+}//DbConnection.
