e071d9b6a5d34bb6ff2db0388202f835bff0b40c
Nick Christidis <nick.christidis@yahoo.com>
2014-06-19 16:45:07 +0300

    Merge origin/master
    Conflicts:
    	src/database/connection/DbConnection.java

--- src/database/daos/EkdotesDAO.java
+++ src/database/daos/EkdotesDAO.java
@@ -1,10 +1,10 @@
 package database.daos;
 
-import database.connection.DbConnection;
 import database.models.Ekdotis;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -13,65 +13,54 @@ import java.util.ArrayList;
  * @author nikos
  */
 public class EkdotesDAO {
-    
-    private DbConnection db;
-    private static Connection con;
 
-    public EkdotesDAO() {
+    private final Connection con;
+
+    public EkdotesDAO(Connection conn) {
+        this.con = conn;
     }
-    
+
     //----------------------------------------------------------------------------------    
-    public void insertEkdoti(Ekdotis ekdotis){
-        
-        db = new DbConnection();
-        con = db.getConn();
-        
-        try
-        {
+    public void insertEkdoti(Ekdotis ekdotis) {
+
+        try {
             String sql = "INSERT INTO sistima_vivliothikis_ergasia.ekdotes (onoma_ekdoti) VALUES (?)";
-            PreparedStatement ps = con.prepareStatement(sql);            
+            PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, ekdotis.getName());
-                       
-            ps.executeUpdate();  
+
+            ps.executeUpdate();
             ps.close();
             con.close();
-        }
-        catch(Exception e)
-        {
+        } catch (SQLException e) {
             System.out.println(e + "     EkdotesDAO.insertEkdoti()");
-        }         
+        }
     }
 //----------------------------------------------------------------------------------
-    public ArrayList<Ekdotis> findAll(){
-        db = new DbConnection();
-        con = db.getConn();
-        
+
+    public ArrayList<Ekdotis> findAll() {
+
         ArrayList allPublishers = new ArrayList();
         Ekdotis ekdotis;
-        try
-        {            
+        try {
             String sql = "SELECT * FROM sistima_vivliothikis_ergasia.eksotes ORDER BY onoma_ekdoti";
             Statement s = con.createStatement();
-            ResultSet rs = s.executeQuery(sql);                       
-                        
-            while(rs.next())
-            {
+            ResultSet rs = s.executeQuery(sql);
+
+            while (rs.next()) {
                 ekdotis = new Ekdotis();
-                
+
                 ekdotis.setId(Integer.parseInt(rs.getString("id_ekdoti")));
                 ekdotis.setName(rs.getString("onoma_ekdoti"));
-                                                              
+
                 allPublishers.add(ekdotis);
-            }    
+            }
             s.close();
             rs.close();
             con.close();
-        }
-        catch(Exception e)
-        {
+        } catch (NumberFormatException | SQLException e) {
             System.out.println(e + "       EkdotesDAO.findAll()");
         }
-        
+
         return allPublishers;
     }
 //----------------------------------------------------------------------------------   
