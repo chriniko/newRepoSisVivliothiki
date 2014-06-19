4a94c77330900fb27880340febd71ddc3018bc46
Akis <Akis@Akis-PC.lan>
2014-06-19 11:07:23 +0300

    Merge origin/master
    Conflicts:
    	src/database/models/Antitypo.java
    	src/database/models/Melos.java
    	src/database/models/Vivlio.java

--- src/database/connection/DbConnection.java
+++ src/database/connection/DbConnection.java
@@ -1,6 +1,8 @@
 package database.connection;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -8,11 +10,51 @@ import com.mysql.jdbc.Connection;
  */
 public class DbConnection {
 
+    /*
     private static final String DB_URL = "";
     private static final String DB_USER = "root";
     private static final String DB_PASS = "nikos";
-
-    private DbConnection() {
+    */
+    
+    private static final String db_user= "root";
+    private static final String db_pass= "a7777777";
+    private static final String url = "jdbc:mysql://localhost:3306/mysql";
+    private Connection con;    
+    
+    
+    //constructor :
+    public DbConnection(){        
+        
     }
-
+    
+    
+    public Connection getConn(){        
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+	} 
+        catch(java.lang.ClassNotFoundException e) 
+        {
+            System.err.print("ClassNotFoundException: ");
+            System.err.println(e.getMessage());
+	}
+ 	try 
+        {
+            con = DriverManager.getConnection(url, db_user, db_pass);
+	} 
+        catch(SQLException ex) 
+        {
+            System.err.println("SQLException: " + ex.getMessage() + "     DBConnection.getConn");
+	}
+	return con;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }//DbConnection.
--- src/database/daos/MelhDAO.java
+++ src/database/daos/MelhDAO.java
@@ -1,9 +1,114 @@
 package database.daos;
 
+import database.connection.DbConnection;
+import database.models.Melos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author nikos
  */
 public class MelhDAO {
+
+    private DbConnection db;
+    private static Connection con;
+    
+    public MelhDAO() {
+    }
+//----------------------------------------------------------------------------------    
+    public void insertMelos(Melos melos){
+        
+        db = new DbConnection();
+        con = db.getConn();
+        
+        try
+        {
+            String sql = "INSERT INTO sistima_vivliothikis_ergasia.melh (am_melous, onoma_melous, epitheto_melous, email_melous, pass_melous) VALUES (?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, String.valueOf(melos.getAm()));
+            ps.setString(2, melos.getOnoma());
+            ps.setString(3, melos.getEpitheto());
+            ps.setString(4, melos.getEmail());
+            ps.setString(5, melos.getPass());                              
+            
+            ps.executeUpdate();            
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + "     MelhDAO.insertMelos()");
+        }         
+    }
+//----------------------------------------------------------------------------------    
+    public Melos searchMelosByAm(int am){
+        
+        Melos melos = new Melos();
+        try
+        {            
+            String sql = "SELECT * FROM sistima_vivliothikis_ergasia.melh WHERE am_melous LIKE '"+am+"'";
+            Statement s = con.createStatement();
+            ResultSet rs = s.executeQuery(sql);                       
+                        
+            while(rs.next())
+            {
+                melos.setAm(Integer.parseInt(rs.getString("am_melous")));
+                melos.setOnoma(rs.getString("onoma_melous"));
+                melos.setEpitheto(rs.getString("epitheto_melous"));
+                melos.setEmail(rs.getString("email_melous"));
+                melos.setPass(rs.getString("pass_melous"));                                                                                          
+            }            
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + "     MelhDAO.searchMelosByAM()");
+        }
+    
+        return melos;
+    }
+//----------------------------------------------------------------------------------    
+    public Melos searchMelosByEpitheto(String epitheto){
+        
+        Melos melos = new Melos();
+        try
+        {            
+            String sql = "SELECT * FROM sistima_vivliothikis_ergasia.melh WHERE epitheto_melous LIKE '"+epitheto+"'";
+            Statement s = con.createStatement();
+            ResultSet rs = s.executeQuery(sql);                       
+                        
+            while(rs.next())
+            {
+                melos.setAm(Integer.parseInt(rs.getString("am_melous")));
+                melos.setOnoma(rs.getString("onoma_melous"));
+                melos.setEpitheto(rs.getString("epitheto_melous"));
+                melos.setEmail(rs.getString("email_melous"));
+                melos.setPass(rs.getString("pass_melous"));                                                                                          
+            }            
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + "     MelhDAO.searchMelosByEpitheto()");
+        }    
+        return melos;
+    }
+//----------------------------------------------------------------------------------    
+    
+    
+    
+    
+    
+    
+/*    
+    public static void main(String[] args)
+{
+    Melos m = new Melos();
+    m.setAm(1);m.setOnoma("akis"); m.setEpitheto("akissss"); m.setEmail("@hotmail.com"); m.setPass("12345");
+    
+    MelhDAO dao = new MelhDAO();
+    dao.insertMelos(m);
+}
+  */  
+    
     
 }//MelhDAO.
