efcfb8772ad122583d79037ed3cb90351cdb6342
Nick Christidis <nick.christidis@yahoo.com>
2014-06-26 21:13:43 +0300

    MHN KANEIS PUSH 'H PULL!!!!

--- src/database/daos/AntitypaDAO.java
+++ src/database/daos/AntitypaDAO.java
@@ -16,12 +16,25 @@ public class AntitypaDAO {
     private PreparedStatement pStat;
     private ResultSet rs;
     private final Connection conn;
-    private static final String ANAKTISI_ANTITYPWN_VIVLIOU = "SELECT * FROM antitypa WHERE vivlia_isbn_vivliou = ?";
+    //=======================================================================================================
+    private static final String ANAKTISI_ANTITYPWN_VIVLIOU = "SELECT * FROM antitypa WHERE vivlia_isbn_vivliou = ? ORDER BY 1,2,3 ASC";
+    private static final String ANAKTISI_DIATHESIMWN_ANT_VIVLIOU = "SELECT * FROM antitypa WHERE am_daneismenou_melous IS NULL AND vivlia_isbn_vivliou = ? "
+            + "ORDER BY 1,2,3 ASC";
+    private static final String ANAKTISI_MI_DIATHESIMWN_ANT_VIVLIOU = "SELECT * FROM antitypa WHERE am_daneismenou_melous IS NOT NULL AND vivlia_isbn_vivliou = ? "
+            + "ORDER BY 1,2,3 ASC";
+    //=======================================================================================================
+    private static final String ANAKTISI_PLITHOUS_ANTITYPWN_VIVLIOU = "SELECT COUNT(*) FROM antitypa WHERE vivlia_isbn_vivliou = ?";
+    private static final String ANAKTISI_PLITHOUS_DIATHESIMWN_ANT_VIVLIOU = "SELECT COUNT(*) FROM antitypa WHERE am_daneismenou_melous IS NULL AND vivlia_isbn_vivliou = ? ";
+    private static final String ANAKTISI_PLITHOUS_MI_DIATHESIMWN_ANT_VIVLIOU = "SELECT COUNT(*) FROM antitypa WHERE am_daneismenou_melous IS NOT NULL AND vivlia_isbn_vivliou = ? ";
+    //=======================================================================================================
+    private static final String ANAKTISI_DANEISMENWN_ANTITYPWN_MELOUS = "SELECT * FROM antitypa WHERE am_daneismenou_melous = ? ORDER BY 1,2,3 ASC";
+    //=======================================================================================================
 
     public AntitypaDAO(Connection conn) {
         this.conn = conn;
     }//ctor.
 
+    //========================================================================================================
     public ArrayList<Antitypo> anaktisiAntitypwnVivliou(String isbnVivliou) {
         try {
 
@@ -66,17 +79,208 @@ public class AntitypaDAO {
     }//anaktisiAntitypwnVivliou.
 
     public ArrayList<Antitypo> anaktisiDiathesimwnAntitypwnVivliou(String isbnVivliou) {
-        //TODO add code...
+        try {
+
+            pStat = conn.prepareStatement(ANAKTISI_DIATHESIMWN_ANT_VIVLIOU);
+            pStat.setString(1, isbnVivliou);
+
+            rs = pStat.executeQuery();
+
+            ArrayList<Antitypo> antitypa = new ArrayList<>();
+            Antitypo temp;
+
+            while (rs.next()) {
+
+                temp = new Antitypo();
+                temp.setIsbnVivliou(rs.getString("vivlia_isbn_vivliou"));
+                temp.setIdAntitypou(rs.getInt("id_antitypou"));
+                temp.setKatastasiAntitypou(rs.getString("katastasi_antitypou"));
+                temp.setAm_daneismenou_melous(rs.getInt("am_daneismenou_melous"));
+                temp.setHmnia_daneismou(rs.getDate("hmnia_daneismou"));
+
+                antitypa.add(temp);
+            }//while.
+
+            return antitypa;
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                if (!pStat.isClosed()) {
+                    pStat.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
         return null;
     }//anaktisiDiathesimwnAntitypwnVivliou.
 
     public ArrayList<Antitypo> anaktisiDaneismenwnAntitypwnVivliou(String isbnVivliou) {
-        //TODO add code...
+        try {
+
+            pStat = conn.prepareStatement(ANAKTISI_MI_DIATHESIMWN_ANT_VIVLIOU);
+            pStat.setString(1, isbnVivliou);
+
+            rs = pStat.executeQuery();
+
+            ArrayList<Antitypo> antitypa = new ArrayList<>();
+            Antitypo temp;
+
+            while (rs.next()) {
+
+                temp = new Antitypo();
+                temp.setIsbnVivliou(rs.getString("vivlia_isbn_vivliou"));
+                temp.setIdAntitypou(rs.getInt("id_antitypou"));
+                temp.setKatastasiAntitypou(rs.getString("katastasi_antitypou"));
+                temp.setAm_daneismenou_melous(rs.getInt("am_daneismenou_melous"));
+                temp.setHmnia_daneismou(rs.getDate("hmnia_daneismou"));
+
+                antitypa.add(temp);
+            }//while.
+
+            return antitypa;
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                if (!pStat.isClosed()) {
+                    pStat.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
         return null;
     }//anaktisiDaneismenwnAntitypwnVivliou.
 
-    public ArrayList<Antitypo> anaktisiAntitypwnMelous(int am_melous) {
-        //TODO add code...
+    //==========================================================================================================
+    public int anaktisiPlithousAntitypwnVivliou(String isbn) {
+        int res = 0;
+        try {
+            pStat = conn.prepareStatement(ANAKTISI_PLITHOUS_ANTITYPWN_VIVLIOU);
+            pStat.setString(1, isbn);
+
+            rs = pStat.executeQuery();
+            rs.first();
+            res = rs.getInt(1);
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                if (!pStat.isClosed()) {
+                    pStat.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return res;
+    }//anaktisiPlithousAntitypwnVivliou.
+
+    public int anaktisiPlithousDiathesimwnAntitypwnVivliou(String isbn) {
+        int res = 0;
+        try {
+            pStat = conn.prepareStatement(ANAKTISI_PLITHOUS_DIATHESIMWN_ANT_VIVLIOU);
+            pStat.setString(1, isbn);
+
+            rs = pStat.executeQuery();
+            rs.first();
+            res = rs.getInt(1);
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                if (!pStat.isClosed()) {
+                    pStat.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return res;
+    }//anaktisiPlithousDiathesimwnAntitypwnVivliou.
+
+    public int anaktisiPlithousMiDiathesimwnAntitypwnVivliou(String isbn) {
+        int res = 0;
+        try {
+            pStat = conn.prepareStatement(ANAKTISI_PLITHOUS_MI_DIATHESIMWN_ANT_VIVLIOU);
+            pStat.setString(1, isbn);
+
+            rs = pStat.executeQuery();
+            rs.first();
+            res = rs.getInt(1);
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                if (!pStat.isClosed()) {
+                    pStat.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return res;
+    }//anaktisiPlithousMiDiathesimwnAntitypwnVivliou.
+
+    //========================================================================================================
+    public ArrayList<Antitypo> anaktisiAntitypwnMelousPouExeiDaneistei(int am_melous) {
+        try {
+
+            pStat = conn.prepareStatement(ANAKTISI_DANEISMENWN_ANTITYPWN_MELOUS);
+            pStat.setInt(1, am_melous);
+
+            rs = pStat.executeQuery();
+
+            ArrayList<Antitypo> antitypa = new ArrayList<>();
+            Antitypo temp;
+
+            while (rs.next()) {
+                temp = new Antitypo();
+
+                temp.setIsbnVivliou(rs.getString("vivlia_isbn_vivliou"));
+                temp.setIdAntitypou(rs.getInt("id_antitypou"));
+                temp.setKatastasiAntitypou(rs.getString("katastasi_antitypou"));
+                temp.setAm_daneismenou_melous(rs.getInt("am_daneismenou_melous"));
+                temp.setHmnia_daneismou(rs.getDate("hmnia_daneismou"));
+
+                antitypa.add(temp);
+            }//while.
+
+            return antitypa;
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                if (!pStat.isClosed()) {
+                    pStat.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
         return null;
     }//anaktisiAntitypwnMelous.
 
--- src/database/daos/IstorikoMelousDAO.java
+++ src/database/daos/IstorikoMelousDAO.java
@@ -1,9 +1,68 @@
 package database.daos;
 
+import database.models.IstorikoMelous;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
 /**
  *
  * @author nikos
  */
 public class IstorikoMelousDAO {
+
+    private final Connection conn;
+    private PreparedStatement pStat;
+    private ResultSet rs;
+    //===============================================================================
+    private static final String ANAKTISI_ISTORIKOU_MELOUS = "SELECT * FROM istoriko_melous WHERE melh_am_melous = ?";
+
+    //================================================================================
+    public IstorikoMelousDAO(Connection conn) {
+        this.conn = conn;
+    }//ctor.
+
+    public LinkedList<IstorikoMelous> anaktisiIstorikouMelous(int amMelous) {
+        try {
+            pStat = conn.prepareStatement(ANAKTISI_ISTORIKOU_MELOUS);
+            pStat.setInt(1, amMelous);
+            rs = pStat.executeQuery();
+
+            LinkedList<IstorikoMelous> istoriko = new LinkedList<>();
+            IstorikoMelous temp;
+
+            while (rs.next()) {
+
+                temp = new IstorikoMelous();
+
+                temp.setIsbnVivliou(rs.getString("antitypa_vivlia_isbn_vivliou"));
+                temp.setIdAntitypou(rs.getInt("antitypa_id_antitypou"));
+                temp.setAmMelous(rs.getInt("melh_am_melous"));
+                temp.setHmniaDaneismou(rs.getDate("hmnia_daneismou"));
+                temp.setHmniaEpistrofis(rs.getDate("hmnia_epistrofis"));
+
+                istoriko.add(temp);
+            }//while.
+
+            return istoriko;
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+
+            try {
+                if (!pStat.isClosed()) {
+                    pStat.close();
+                }
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return null;
+    }//anaktisiIstorikouMelous.
 
 }//IstorikoMelousDAO.
